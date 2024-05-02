f648e6d4942ce611a31202d3c158a2167251d65f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-02 21:34:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Fade } from "react-awesome-reveal";
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 const HomePage = () => {
     // https://wallpapercave.com/wp/wp6367798.jpg
   return (
     <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp6367798.jpg)] bg-center bg-cover lg:h-screen h-screen w-full'>
-        HI all
+        <Navbar />
+        <div className="">
+            HI all
+        </div>
 
-        <div className="inline-block align-bottom">
+        <div className="">
             <Footer />
         </div>
     </div>
