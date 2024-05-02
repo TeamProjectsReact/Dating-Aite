87392d2f34c8c8e5cf318da8a4c66c2699a8df2b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-02 21:31:27 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Fade } from "react-awesome-reveal";
+import Footer from './Footer';
 
 const HomePage = () => {
     // https://wallpapercave.com/wp/wp6367798.jpg
   return (
-    <div>
-        <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp6367798.jpg)] bg-center bg-cover lg:h-screen h-screen w-full'>
-            HI all
+    <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp6367798.jpg)] bg-center bg-cover lg:h-screen h-screen w-full'>
+        HI all
+
+        <div className="inline-block align-bottom">
+            <Footer />
         </div>
-        {/* <Fade  cascade damping={0.1}>
-            <p>I will gently appear as I enter the viewport</p>
-        </Fade> */}
     </div>
   )
 }
