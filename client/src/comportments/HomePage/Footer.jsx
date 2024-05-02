de21ddd936a454622475d5921010cf3fdbbb2293
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-02 21:43:37 +0530

    UPdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Footer.jsx
+++ client/src/comportments/HomePage/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-white'>
+    <footer className='text-white'>
         Footer
-    </div>
+    </footer>
   )
 }
 
--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ import Navbar from './Navbar';
 const HomePage = () => {
     // https://wallpapercave.com/wp/wp6367798.jpg
   return (
-    <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp6367798.jpg)] bg-center bg-cover lg:h-screen h-screen w-full'>
+    <div className=' flex flex-col min-h-screen lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp6367798.jpg)] bg-center bg-cover lg:h-screen h-screen w-full'>
         <Navbar />
         <div className="py-40">
             hi al
