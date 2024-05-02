92fceeb2cce190dd7bf6422e99771fabb59c5625
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-02 21:24:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Fade   } from "react-awesome-reveal";
 
 const HomePage = () => {
+    // https://wallpapercave.com/wp/wp3091867.jpg
   return (
-    <div>HomePage</div>
+    <div>
+        <div className='lg:py-[4%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/wp3091867.jpg)] bg-center bg-cover lg:h-[80vh] h-screen w-full'>
+            HI all
+        </div>
+        <Fade  cascade damping={0.1}>
+            <p>I will gently appear as I enter the viewport</p>
+        </Fade>
+    </div>
   )
 }
 
