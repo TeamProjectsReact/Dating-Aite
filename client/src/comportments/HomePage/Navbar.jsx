4df0e71a5ac85133eb9121e000ce1c54b39a9ebd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-04 09:20:30 +0530

    Update Navbar.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Navbar.jsx
+++ client/src/comportments/HomePage/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import LoveImage from '../../assets/lvoe.png'
 
 const Navbar = () => {
   return (
     <div>
         <div className='mb-20 text-2xl font-semibold text-white'>
-            Navbar
+            <img src={LoveImage} alt="" className='w-50 h-50'/>
         </div>
     </div>
   )
