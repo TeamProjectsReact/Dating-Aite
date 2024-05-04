bf6f236033fcbc3fb51bf85453be8cfdba613c59
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-04 09:26:21 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ import Navbar from './Navbar';
 const HomePage = () => {
     // https://wallpapercave.com/wp/dxK9FJ3.jpg
   return (
-    <div className='lg:py-[3%] py-12 lg:px-16 bg-[url(https://wallpapercave.com/wp/dxK9FJ3.jpg)] lg:h-[80vh] h-screen w-full bg-center bg-cover lg:h-screen h-screen w-full'>
+    <div className='lg:py-[3%] py-12 lg:px-16 px-8 bg-[url(https://wallpapercave.com/wp/dxK9FJ3.jpg)] lg:h-[80vh] h-screen w-full lg:bg-center bg-right bg-cover lg:h-screen h-screen w-full'>
         <Navbar />
         <div className="">
             
