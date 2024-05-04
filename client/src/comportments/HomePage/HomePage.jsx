345c4f1aab84ea280c33dbbf8f403fbd7f751a10
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-04 09:30:24 +0530

    Update HomePage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/HomePage.jsx
+++ client/src/comportments/HomePage/HomePage.jsx
@@ -6,14 +6,17 @@ import Navbar from './Navbar';
 const HomePage = () => {
     // https://wallpapercave.com/wp/dxK9FJ3.jpg
   return (
-    <div className='lg:py-[3%] py-12 lg:px-16 px-8 bg-[url(https://wallpapercave.com/wp/dxK9FJ3.jpg)] lg:h-[80vh] h-screen w-full lg:bg-center bg-right bg-cover lg:h-screen h-screen w-full'>
+    <div className="">
+      <div className='lg:py-[3%] py-12 lg:px-16 px-8 bg-[url(https://wallpapercave.com/wp/dxK9FJ3.jpg)] lg:h-[80vh] h-screen w-full lg:bg-center bg-right bg-cover lg:h-screen h-screen w-full'>
         <Navbar />
         <div className="">
-            
+            <h1 className="lg:mx-16 mx-0 text-white text-5xl">Find your <span className='text-pink-500'>best trusted</span> <br /> and <span className='text-pink-500'>lovelyness</span> partner</h1>
         </div>
 
 
-        <Footer />
+        
+      </div>
+      <Footer />
     </div>
   )
 }
