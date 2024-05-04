c880dab288fcc38876e317887a3e4e2120002351
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-04 09:21:55 +0530

    Update Navbar.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomePage/Navbar.jsx
+++ client/src/comportments/HomePage/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className='mb-20 text-2xl font-semibold text-white'>
           <div className="flex">
             <img src={LoveImage} alt="" className='w-20 h-auto'/>
-            <p className="">Dating App</p>
+            <p className="mt-2">Dating App</p>
           </div>
         </div>
     </div>
