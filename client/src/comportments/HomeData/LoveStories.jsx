1c54025d34fbeb0798d68d2ae6fb8771976d952b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 15:30:28 +0530

    Update LoveStories.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomeData/LoveStories.jsx
+++ client/src/comportments/HomeData/LoveStories.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LoveStories = () => {
   return (
     <div className=''>
-        <h1 className="text-5xl love-title text-pink-500 my-4 text-center">Girls</h1>
+        <h1 className="text-5xl love-title text-pink-500 my-4 text-center">Love Stories</h1>
     </div>
   )
 }
