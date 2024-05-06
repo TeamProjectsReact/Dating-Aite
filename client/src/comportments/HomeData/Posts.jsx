55ed57170d0ff5b4b9894d92fe77a04f6a27b72c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 15:31:24 +0530

    Create Posts.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/comportments/HomeData/Posts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Posts = () => {
+  return (
+    <div className=''>
+        <h1 className="text-5xl love-title text-pink-500 my-4 text-center">Posts</h1>
+    </div>
+  )
+}
+
+export default Posts
