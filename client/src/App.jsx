aa0087bc157b9883873d262c77214b84f3488d36
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 18:38:17 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./comportments/HomePage/HomePage";
 import Dashboard from "./comportments/Dashboard/Dashboard";
+import PrivateRoute from "./comportments/Security/PrivateRoute";
 
 
 export default function App() {
@@ -9,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
 
-        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
   )
