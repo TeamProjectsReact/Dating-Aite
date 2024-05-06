6c239c8ef6f002bd3c9a7ee3597f69358f324f6e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 15:36:53 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/SignInSignUp/SignIn.jsx
+++ client/src/comportments/SignInSignUp/SignIn.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const SignIn = () => {
   return (
     <div>
-        .
+        <div className="my-2">
+            <form>
+                <label htmlFor="">Username : </label>
+                <input type="text" name="" id="" className="w-full h-12 border border-pink-500" />
+            </form>
+        </div>
     </div>
   )
 }
