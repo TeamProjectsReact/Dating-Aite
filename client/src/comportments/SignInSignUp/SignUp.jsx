5387f020eb2ceb1574ef1831cdf4205947bbf645
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 16:08:53 +0530

    Update SignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/SignInSignUp/SignUp.jsx
+++ client/src/comportments/SignInSignUp/SignUp.jsx
@@ -11,12 +11,16 @@ const SignUp = () => {
         password: ''
      })
 
+     const headleSubmit = (e) => {
+        e.preventDefault()
+     }
+
   return (
     <div>
         <div className="my-20 py-16 px-12 rounded-xl bg-white">
             <span onClick={headleBack} className="text-pink-500 cursor-pointer duration-500 hover:ml-2">Back to Home</span>
             <h1 className="love-title text-pink-500 text-center text-2xl my-2">Create New Account ?</h1>
-            <form>
+            <form onSubmit={headleSubmit}>
                 <div className="my-4">
                     <label htmlFor="" className='text-pink-500 my-2'>Username : </label>
                     <input type="text" name="" id="" className="pl-2 w-full h-12 border border-pink-500 rounded text-pink-500" required placeholder='Enter Username' 
