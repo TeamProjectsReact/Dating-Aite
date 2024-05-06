db16e22969d48065af6a509c144ca2b97a0d39d2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 13:35:27 +0530

    Update Girls.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomeData/Girls.jsx
+++ client/src/comportments/HomeData/Girls.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Girls = () => {
   return (
-    <div>Girls</div>
+    <div className='bg-pink-100 py-8 px-12 rounded shadow-md'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione iure fuga dolores corrupti ea eveniet eum excepturi? Aliquid ipsam illo nisi vero ad asperiores quia tempora odio.
+    </div>
   )
 }
 
