5e8f937b9003dc6f91ba63a9d31bb8f3824467eb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 20:28:31 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/Dashboard.jsx
+++ client/src/comportments/Dashboard/Dashboard.jsx
@@ -4,6 +4,21 @@ import { Link, useNavigate } from "react-router-dom"
 import  secureLocalStorage  from  "react-secure-storage"
 
 const Dashboard = () => {
+  const navigate = useNavigate() 
+
+  //for open and close Side bar
+  const [sideOpen, SetsideOpen] = useState();
+  const [navOpen, SetNavOpen] = useState();
+
+  //curent login user
+  const RoleUser = secureLocalStorage.getItem("Login1");
+  const EmailUser = secureLocalStorage.getItem("login2");
+
+  const [buttonValue, SetButtonValue] = useState(0)
+  const HeadleButtonClick = (clickValue) => {
+      SetButtonValue(clickValue)   
+  }
+  
   return (
     <div>Dashboard</div>
   )
