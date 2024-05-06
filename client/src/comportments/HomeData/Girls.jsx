6dce061fb3914c8fbb4b0124cb93fc5c9856970d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 13:37:04 +0530

    Update Girls.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomeData/Girls.jsx
+++ client/src/comportments/HomeData/Girls.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const Girls = () => {
+    const girls = [
+        {name: "Nimali", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "", age: "24"},
+        {name: "Nimali", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "", age: "24"},
+        {name: "Nimali", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "", age: "24"},
+        {name: "Nimali", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "", age: "24"},
+        {name: "Nimali", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "", age: "24"},
+    ]
+
   return (
     <div className='bg-pink-100 py-8 px-12 rounded shadow-md'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione iure fuga dolores corrupti ea eveniet eum excepturi? Aliquid ipsam illo nisi vero ad asperiores quia tempora odio.
