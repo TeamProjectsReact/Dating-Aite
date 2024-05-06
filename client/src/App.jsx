76d68ca5e176ec541b943284bc9d5e623019290c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 13:10:07 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./comportments/HomePage/HomePage";
-import DarkModeTest from "./comportments/Testings/DarkModeTest";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/DarkModeTest" element={<DarkModeTest />} />
       </Routes>
     </BrowserRouter>
   )
--- client/src/comportments/Testings/DarkModeTest.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DarkModeTest = () => {
-  return (
-    <div>DarkModeTest</div>
-  )
-}
-
-export default DarkModeTest
