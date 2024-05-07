506a7877083090ca22121ca7ad7a0b768360a91c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-07 13:04:19 +0530

    Update MyData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyData/MyData.jsx
+++ client/src/comportments/MyData/MyData.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const MyData = () => {
-  return (
-    <div>MyData</div>
-  )
+    const navigate = useNavigate() 
+
+    //for open and close Side bar
+    const [sideOpen, SetsideOpen] = useState();
+    const [navOpen, SetNavOpen] = useState();
+  
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+  
+    const [buttonValue, SetButtonValue] = useState(0)
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+
+    if(RoleUser !== null && EmailUser !== null){
+        return (
+            <div>MyData</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default MyData
