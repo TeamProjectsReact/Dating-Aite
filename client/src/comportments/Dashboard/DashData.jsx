8d7004cb882b7444faf676b270b739b204f91356
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-07 13:02:54 +0530

    Update DashData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/DashData.jsx
+++ client/src/comportments/Dashboard/DashData.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom"
 import  secureLocalStorage  from  "react-secure-storage"
 import CountUp from 'react-countup';
 import axios from "axios";
+import MyData from "../MyData/MyData";
 
 
 const DashData = () => {
@@ -41,93 +42,101 @@ const DashData = () => {
   ]
 
     if(RoleUser !== null && EmailUser !== null){
-      return (
-        <div>
-          <div className="">
-            {/* {isUpdateData.Data} */}
-
-          </div>
-          {
-            (() => {
-              if(RoleUser === "SuperAdmin"){
-                return (
-                  <h1 className="text-pink-500 text-xl font-semibold mx-4">SuperAdmin Dashboard</h1>
-                )
-              }
-              else if(RoleUser === "user"){
-                return (
-                  <h1 className="text-pink-500 text-xl font-semibold mx-4">User Dashboard</h1>
-                )
-              }
-            })()
-          }
-
-          <div className="lg:grid grid-cols-4 gap-4 my-8 mx-4">
-              {
-                allDataCout.map((dashData) => {
-                  if(RoleUser === "SuperAdmin"){
-                    return (
-                      <div onClick={() => HeadleButtonClick(dashData.btnValue)} className="bg-pink-400 text-white rounded-xl text-center py-8 lg:px-8 px-0 cursor-pointer duration-500 hover:bg-pink-500">
-                        <h1 className="text-3xl"><Icons name={dashData.icon} size="large"></Icons></h1>
-                        <h1 className="text-md">{dashData.name}</h1>
-                        <h1 className="text-xl">{dashData.value}</h1>
-                      </div>
-                    )
-                  }
-                  else if(RoleUser === "user"){
-                    if(dashData.id !== 6 && dashData.id !== 7){
+      if(buttonValue === 0){
+        return (
+          <div>
+            <div className="">
+              {/* {isUpdateData.Data} */}
+  
+            </div>
+            {
+              (() => {
+                if(RoleUser === "SuperAdmin"){
+                  return (
+                    <h1 className="text-pink-500 text-xl font-semibold mx-4">SuperAdmin Dashboard</h1>
+                  )
+                }
+                else if(RoleUser === "user"){
+                  return (
+                    <h1 className="text-pink-500 text-xl font-semibold mx-4">User Dashboard</h1>
+                  )
+                }
+              })()
+            }
+  
+            <div className="lg:grid grid-cols-4 gap-4 my-8 mx-4">
+                {
+                  allDataCout.map((dashData) => {
+                    if(RoleUser === "SuperAdmin"){
                       return (
-                        <div onClick={() => HeadleButtonClick(dashData.btnValue)} className="bg-pink-400 text-white rounded-xl text-center py-8 px-8 cursor-pointer duration-500 hover:bg-pink-500">
+                        <div onClick={() => HeadleButtonClick(dashData.btnValue)} className="bg-pink-400 text-white rounded-xl text-center py-8 lg:px-8 px-0 cursor-pointer duration-500 hover:bg-pink-500">
                           <h1 className="text-3xl"><Icons name={dashData.icon} size="large"></Icons></h1>
                           <h1 className="text-md">{dashData.name}</h1>
                           <h1 className="text-xl">{dashData.value}</h1>
                         </div>
                       )
                     }
-                  }
-
-                })
-              }
-          </div>
-          {/* {buttonValue} */}
-          <div className="">
-            {/* fetch data */}
-          </div>
-
-          <div className="my-8">
-            <h1 className="text-pink-500 text-xl font-semibold mx-4">Personal Information</h1>
-
-            <div className="mx-4 rounded-xl my-4 bg-pink-200 py-8 px-8">
-              <div className="lg:grid grid-cols-3 gap-4">
-                <div className="">
-                  <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" className=" ml-4"/>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Username: </span> Jehankandy</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Email: </span> jehan@123.com</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">First Name: </span> Jehankandy</p>
-                </div>
-                <div className="">
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Last Name: </span> kandy</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">NIC/Passport No: </span> 123456789V</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Address: </span> 88, ABC Street, XYZ</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Mobile: </span> +94 xxxxxxxxx</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">location: </span> USA</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Gender: </span> Male</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Partners: </span> Female</p>
-                </div>
-                <div className="">
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Accept Pets: </span> No</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Accept Drinks: </span> Yes</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Covid Vaccinated: </span> Yes</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Accept Dating for: </span> 5 days</p>
-                  <p className="my-2"><span className="text-pink-500 font-semibold">Accept : </span> Yes</p>
-                  <button className="bg-pink-400 py-4 px-8 text-white rounded-xl duration-500 hover:ml-2">Update</button>
-                </div>
-              </div>
-
+                    else if(RoleUser === "user"){
+                      if(dashData.id !== 6 && dashData.id !== 7){
+                        return (
+                          <div onClick={() => HeadleButtonClick(dashData.btnValue)} className="bg-pink-400 text-white rounded-xl text-center py-8 px-8 cursor-pointer duration-500 hover:bg-pink-500">
+                            <h1 className="text-3xl"><Icons name={dashData.icon} size="large"></Icons></h1>
+                            <h1 className="text-md">{dashData.name}</h1>
+                            <h1 className="text-xl">{dashData.value}</h1>
+                          </div>
+                        )
+                      }
+                    }
+  
+                  })
+                }
             </div>
-          </div>          
-        </div>
-      )
+            {/* {buttonValue} */}
+            <div className="">
+              {/* fetch data */}
+            </div>
+  
+            <div className="my-8">
+              <h1 className="text-pink-500 text-xl font-semibold mx-4">Personal Information</h1>
+  
+              <div className="mx-4 rounded-xl my-4 bg-pink-200 py-8 px-8">
+                <div className="lg:grid grid-cols-3 gap-4">
+                  <div className="">
+                    <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" className=" ml-4"/>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Username: </span> Jehankandy</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Email: </span> jehan@123.com</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">First Name: </span> Jehankandy</p>
+                  </div>
+                  <div className="">
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Last Name: </span> kandy</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">NIC/Passport No: </span> 123456789V</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Address: </span> 88, ABC Street, XYZ</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Mobile: </span> +94 xxxxxxxxx</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">location: </span> USA</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Gender: </span> Male</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Partners: </span> Female</p>
+                  </div>
+                  <div className="">
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Accept Pets: </span> No</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Accept Drinks: </span> Yes</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Covid Vaccinated: </span> Yes</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Accept Dating for: </span> 5 days</p>
+                    <p className="my-2"><span className="text-pink-500 font-semibold">Accept : </span> Yes</p>
+                    <button onClick={() => HeadleButtonClick("MyData")} className="bg-pink-400 py-4 px-8 text-white rounded-xl duration-500 hover:ml-2">Update</button>
+                  </div>
+                </div>
+  
+              </div>
+            </div>          
+          </div>
+        )
+      }
+      else if(buttonValue === "MyData"){
+        return (
+          <MyData />
+        )
+      }
+
     }
     else{
       useEffect(() => {
