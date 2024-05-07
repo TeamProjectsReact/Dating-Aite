5a0dc83b2688d768fe78df2e9626cbc29ef36647
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-07 13:16:49 +0530

    Update MyData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/MyData/MyData.jsx
+++ client/src/comportments/MyData/MyData.jsx
@@ -2,6 +2,7 @@ import Icons from "@reacticons/ionicons"
 import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from "react-countup"
 
 const MyData = () => {
     const navigate = useNavigate() 
@@ -23,10 +24,23 @@ const MyData = () => {
         window.location.reload()
     }
 
+    const myData = [
+        {id: 1, name: "My Partners", value: <CountUp end={20} />, icon: "perople", btnvalue: "myPartners"},
+        {id: 2, name: "My Datings", value: <CountUp end={20} />, icon: "perople", btnvalue: "myDatings"},
+        {id: 3, name: "My Stories", value: <CountUp end={20} />, icon: "perople", btnvalue: "myStories"},
+        {id: 4, name: "My Posts", value: <CountUp end={20} />, icon: "perople", btnvalue: "myPosts"},
+        {id: 5, name: "My Reporteds", value: <CountUp end={20} />, icon: "perople", btnvalue: "myReporteds"},
+        
+    ]
+
     if(RoleUser !== null && EmailUser !== null){
         return (
-            <div>
-                <button onClick={headleBack} className="bg-pink-500 text-white py-4 px-8 rounded duration-500 hover:ml-2">Back to Dashboard</button>
+            <div className="">
+                <div>
+                    <button onClick={headleBack} className="bg-pink-500 text-white py-4 px-8 rounded duration-500 hover:ml-2">Back to Dashboard</button>
+                </div>
+
+                {/* my data */}
             </div>
         )
     }
