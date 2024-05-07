8a102c09b4bd2a431ede62c6c51a438b11202814
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-07 08:59:40 +0530

    Update DashData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/Dashboard/DashData.jsx
+++ client/src/comportments/Dashboard/DashData.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import  secureLocalStorage  from  "react-secure-storage"
 
 const DashData = () => {
+    const navigate = useNavigate() 
+
+  //for open and close Side bar
+  const [sideOpen, SetsideOpen] = useState();
+  const [navOpen, SetNavOpen] = useState();
+
+  //curent login user
+  const RoleUser = secureLocalStorage.getItem("Login1");
+  const EmailUser = secureLocalStorage.getItem("login2");
+
+  const [buttonValue, SetButtonValue] = useState(0)
+  const HeadleButtonClick = (clickValue) => {
+      SetButtonValue(clickValue)   
+  }
   return (
     <div>DashData</div>
   )
