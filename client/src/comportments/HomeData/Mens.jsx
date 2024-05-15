279a55b82e00c7e9c730a73fc17aed4fcfac261d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-15 15:04:00 +0530

    Update Mens.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportments/HomeData/Mens.jsx
+++ client/src/comportments/HomeData/Mens.jsx
@@ -2,22 +2,28 @@ import React from 'react'
 
 const Mens = () => {
     const girls = [
-        {name: "Nimali", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
-        {name: "Kamala", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
-        {name: "Perera", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
-        {name: "Amara", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
-        {name: "Sara", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, facere? Veniam ratione", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
+        {name: "Nimali", email: "texting@123.com", mobile: "07xxxxxxx", job: "Software Engineer", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
+        {name: "Kamala", email: "texting@123.com", mobile: "07xxxxxxx", job: "Software Engineer", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
+        {name: "Perera", email: "texting@123.com", mobile: "07xxxxxxx", job: "Software Engineer", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
+        {name: "Amara", email: "texting@123.com", mobile: "07xxxxxxx", job: "Software Engineer", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
+        {name: "Sara", email: "texting@123.com", mobile: "07xxxxxxx", job: "Software Engineer", img: "https://img.freepik.com/free-photo/portrait-smiling-charming-young-man-grey-t-shirt-standing-against-plain-background_23-2148213406.jpg?t=st=1714988150~exp=1714991750~hmac=e1d2db921ee318bd0e8ffd7b7bd6a5021809718f5199a0e4044919a05cba8821&w=740", age: "24"},
     ]
 
   return (
     <div className=''>
         <h1 className="text-5xl love-title text-pink-500 my-4 text-center">Boys</h1>
-        <div className="lg:grid grid-cols-3 gap-4">
+        <div className="lg:grid grid-cols-4 gap-4 my-8">
             {
                 girls.map((girl) => {
                     return (
                         <div className="text-center bg-pink-200 rounded-md shadow-md py-8 px-12 lg:my-0 my-4">
-                            
+                            <div className="flex justify-center items-center ">
+                                <img src={girl.img} alt="" className='lg:my-0 my-24 object-cover rounded-full lg:w-[60%] w-[100%] h-auto'/>
+                            </div>
+                            <h1 className="my-2 font-semibold">{girl.name}</h1>
+                            <h1 className="bg-pink-400 py-1 rounded-2xl text-sm text-white mt-1 mb-3">{girl.job}</h1>
+                            <h1 className="">{girl.mobile}</h1>
+                            <h1 className="">{girl.email}</h1>
                         </div>
                     )
                 })
